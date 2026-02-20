399a20c619697e7586f094dee1e050cff45b4943
mayoralfuenteswendy05-code <mayoralfuenteswendy05@gmail.com>
2026-02-20 01:40:09 -0600

    Delete proyecto-internet/backend directory

--- proyecto-internet/backend/prueba.js
+++
@@ -1,9 +0,0 @@
-const bd = require('./bd').default;
-
-bd.query('SELECT * FROM usuario', (err, results) => {
-  if (err) {
-    console.error('Error en la consulta:', err);
-    return;
-  }
-  console.log('Usuarios:', results);
-});
